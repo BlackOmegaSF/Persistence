81a8cb0c22eb7bf78bb88e3590b18ab94062ecbf
Kian <kian.leiner@gmail.com>
2024-10-04 15:51:35 -0700

    Initial commit

---
+++ src/main/java/com/kleinercode/fabric/persistence/Persistence.java
@@ -0,0 +1,10 @@
+package com.kleinercode.fabric.persistence;
+
+import net.fabricmc.api.DedicatedServerModInitializer;
+
+public class Persistence implements DedicatedServerModInitializer {
+
+    @Override
+    public void onInitializeServer() {
+    }
+}
