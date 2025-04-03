c6445a27cc9acb34954bf1188e49c3ca81de1178
Kian <kian.leiner@gmail.com>
2025-04-02 19:36:18 -0700

    Clean up import

--- src/main/java/com/kleinercode/fabric/persistence/Persistence.java
+++ src/main/java/com/kleinercode/fabric/persistence/Persistence.java
@@ -15,8 +15,6 @@ import net.minecraft.util.ActionResult;
 import net.minecraft.world.GameRules;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.ArrayList;
 import java.util.List;
 
 public class Persistence implements DedicatedServerModInitializer {
