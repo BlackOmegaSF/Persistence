225f33dbdd7e9490f280d82ce96d88b5e069c1c9
Kian <kian.leiner@gmail.com>
2025-04-16 15:43:57 -0700

    Clean up imports

--- src/main/java/com/kleinercode/fabric/persistence/datagen/PersistenceDataGeneratorEntrypoint.java
+++ src/main/java/com/kleinercode/fabric/persistence/datagen/PersistenceDataGeneratorEntrypoint.java
@@ -2,15 +2,6 @@ package com.kleinercode.fabric.persistence.datagen;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
-import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
-import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
-import net.minecraft.data.recipe.RecipeExporter;
-import net.minecraft.data.recipe.RecipeGenerator;
-import net.minecraft.item.Items;
-import net.minecraft.recipe.book.RecipeCategory;
-import net.minecraft.registry.*;
-
-import java.util.concurrent.CompletableFuture;
 
 public class PersistenceDataGeneratorEntrypoint implements DataGeneratorEntrypoint {
 
